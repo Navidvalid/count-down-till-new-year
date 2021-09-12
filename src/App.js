37c7d25feb8c092625cd580be08ea5cf4fa8cd4d
Navid Valid <shahnavid21@gmail.com>
2021-09-12 00:10:43 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react';
+import './style.css'
 
 function App() {
+  
+  const [days , setDays] = useState()
+  const [hours ,setHours] = useState()
+  const [minutes ,setMinutes] = useState()
+  const [seconds ,setSeconds] = useState( )
+
+  const nextYear = new Date().getFullYear() + 1
+  const newYear = `1 Jan ${nextYear}`
+
+  const countDown = () => { 
+    const newYearDate = new Date(newYear)
+    const currentTime = new Date()
+    const totalSeconds = (newYearDate - currentTime) / 1000
+
+    setDays(Math.floor(totalSeconds / 3600 / 24));
+    setHours(Math.floor(totalSeconds  / 3600) % 24);
+    setMinutes(Math.floor(totalSeconds  / 60) % 60);
+    setSeconds(Math.floor(totalSeconds) % 60);
+  }
+
+
+
+  setInterval(countDown, 1000)
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <h3 className="title">Till New Year</h3>
+      <div className="counter-till-new-year">
+        <div className="days-c">
+          <p className='counter'>{days < 10 ? `0${days}` : days}</p>
+          <span>days</span>
+        </div>
+        <div className="days-c">
+          <p className='counter'>{hours < 10 ? `0${hours}` : hours}</p>
+          <span>hours</span>
+        </div>
+        <div className="days-c">
+          <p className='counter'>{minutes < 10 ? `0${minutes}` : minutes}</p>
+          <span>minutes</span>
+        </div>
+        <div className="days-c">
+          <p className='counter'>{ seconds < 10 ? `0${seconds}` : seconds}</p>
+          <span>seconds</span>
+        </div>
+      </div>
     </div>
   );
 }
